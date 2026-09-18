a33ec0923d443f6146eaee9a43b0e263eaf6a721
vulple_io <christophervos522@gmail.com>
2026-09-18 20:43:04 +0200

    Update robots[.]txt.ts

--- src/routes/robots[.]txt.ts
+++ src/routes/robots[.]txt.ts
@@ -52,6 +52,7 @@ export const Route = createFileRoute('/robots.txt')({
         return new Response(robots, {
           headers: {
             'Content-Type': 'text/plain; charset=utf-8',
+            'Cache-Control': 'public, max-age=3600',
           },
         })
       },
