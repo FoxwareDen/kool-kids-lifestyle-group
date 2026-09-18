b3f7e824daefd7064a45674e95da59ed89226e09
vulple_io <christophervos522@gmail.com>
2026-09-18 22:41:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
           if (id.includes('node_modules/lucide-react')) {
             return 'vendor-icons'
           }
+          if (id.includes('node_modules/@tanstack')) {
+            return 'vendor-tanstack'
+          }
         },
       },
     },
